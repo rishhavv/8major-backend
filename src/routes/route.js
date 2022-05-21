e825fac0b4928888ca59af203c63fbdc5d880c3f
rishhavv <rishav.katoch17300@gmail.com>
2022-05-21 15:36:50 +0530

    get routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,12 +2,58 @@ const express = require("express");
 const router = express.Router();
 
 var volunteerArray = [];
+var missingArr = [];
+var communityPosts = [];
 
 router.post("/add-volunteer", (req, res) => {
   try {
     volunteerArray.push(req.body);
     console.log(volunteerArray);
-    res.status(200).send("ss");
+    res.status(200).json({
+      msg: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.get("/volunteer", (req, res) => {
+  try {
+    res.status(200).json({
+      data: volunteerArray,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/report/missing", (req, res) => {
+  try {
+    missingArr.push(req.body);
+    console.log(missingArr);
+    res.status(200).json({
+      msg: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.get("/report/missing", (req, res) => {
+  try {
+    res.status(200).json({
+      data: missingArr,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("community/add", (req, res) => {
+  try {
+    communityPosts.push(req.body);
+    console.log(communityPosts);
+    res.status(200).json({
+      msg: "success",
+    });
   } catch (err) {
     console.log(err);
   }
