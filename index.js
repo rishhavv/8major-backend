4c2957f5adf268fa588484763e49cfe4779a714a
rishhavv <rishav.katoch17300@gmail.com>
2022-05-24 13:42:11 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require("cors");
+
 const app = express();
+app.use(cors());
 
 const volunteer = require("./src/routes/route");
 
